85c8f3052e67d248a174973bfa9ba5d1e77341fc
LAPTOP-UUH16LKD\Fedor <fedor.noskov.99@mail.ru>
2018-04-17 18:40:23 +0300

    To create a boolean matrix from an image

---
+++ sten_func.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import numpy as np
+import os
+
+
+#Создает файл - результат фильтрации последнего бита в байте 
+def pictures_lsb(path, name, ind):
+    path = os.getcwd()
+    container = Image.open(os.path.join(path, name))
+    im_size = container.size
+    im_str = container.tobytes()
+    result = [255*(j % 2**(ind + 1) // 2**ind) for j in im_str]
+    ans = container.frombytes(bytes(result), 'raw')
+    container.save(os.path.join(path, dirrect, 'filtered', 'r_' + name), 
+                   container.format)
+
+
+#Считывает из файла в матрицу из 1 и 0
+def lsb_to_matrix(path, name):
+    container = Image.open(os.path.join(path, name))
+    im_str = container.tobytes()
+    result = np.array([j//255 for j in im_str])
+    result = np.resize(result, (container.size[0]*len(container.mode),
+                               container.size[1]))
+    return result
+
+
+#Каждому байту сопоставляет бит на месте ind
+def picture_to_lsb_matrix(path, name, ind):
+    container = Image.open(os.path.join(path, name))
+    im_size = container.size
+    im_str = container.tobytes()
+    result = np.array([j % 2**(ind + 1) // 2**ind for j in im_str])
+    result = np.resize(result, (container.size[0]*len(container.mode), container.size[1]))
+    return result
