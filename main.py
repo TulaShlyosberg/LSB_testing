6e9e98a0a99b727aea73a423e21ba4a66f94b062
TulaShlyosberg <fedor.noskov.99@mail.ru>
2018-04-24 11:14:34 +0300

    Add files via upload
    New statistics

--- main.py
+++ main.py
@@ -2,156 +2,122 @@ import sten_func as sf
 import os
 import numpy
 import matplotlib.pyplot as plt
-import life
 import compression as cmpr
 
 
+def test_black_square(name):
+    left = 0
+    right = 100
+    height = 100
+    width = 100
+    path = os.path.join(os.getcwd(), 'images')
+    matrix_1 = sf.picture_to_lsb_matrix_colored(path, name, 0, 0)
+    matrix_2 = sf.picture_to_lsb_matrix_colored(path + '\\encoded', 'enc_{}.png'.format(name), 0, 0)
+    freq_1 = sf.black_area_frequencies(matrix_1[0:width, 0:height], left, right)
+    freq_2 = sf.black_area_frequencies(matrix_2[0:width, 0:height], left, right)
+    try:
+        print('E_wout_st = {}'.format(sf.black_area_excpection(freq_1, left, right)))
+    except:
+        print("There aren't black blocks with size between {} and {}, so E_wout_st = 0".format(left, right))
+    try:
+        print('E_w_st = {}'.format(sf.black_area_excpection(freq_2, left, right)))
+    except:
+        print("There aren't black blocks with size between {} and {}, so E_w_st = 0".format(left, right))
+    fig = plt.figure()
+    plt.title('Рапределение черных полей')
+    plt.plot(range(left, right), freq_1, label = 'Without_stego')
+    plt.plot(range(left, right), freq_2, label = 'With_stego')
+    plt.legend()
+    plt.savefig(os.path.join(path + '\\plots', name))
+
+
+def test_life_iterations(name):
+    path = os.path.join(os.getcwd(), 'images')
+    matr_1 = sf.picture_to_lsb_matrix_colored(path, name, 0, 0)
+    matr_2 = sf.picture_to_lsb_matrix_colored(path + '\\encoded', 'enc_' + name, 0, 0)
+    ans_1 = sf.count_statistics(matr_1[0:200, 0:200], 10)
+    ans_2 = sf.count_statistics(matr_2[0:200, 0:200], 10)
+    fig = plt.figure()
+    plt.title('Энтропия Шенона от итераций жизни')
+    plt.plot(range(len(ans_1)), ans_1, label = 'Without stego')
+    plt.plot(range(len(ans_2)), ans_2, label = 'With stego')
+    plt.legend()
+    plt.savefig(path + '\\plots\\life_' + name)
+
+
+
+# Хи-квадрат для каждого элемента разбиения sinica.png и enc_sinica.png
 def test_1():
-    '''
-    Хи-квадрат для каждого элемента разбиения sinica.png и enc_sinica.png
-    :return:
-    '''
     path = os.path.join(os.getcwd(), 'images')
     sf.chi_square_all(path, 'sinica.png', 0)
     path = os.path.join(os.getcwd(), 'images\encoded')
     sf.chi_square_all(path, 'enc_sinica.png', 0)
     return
 
-
+# Шифрование в sinica.png - 30%
 def test_2():
-    '''
-    Шифрование в sinica.png - 30%
-    :return:
-    '''
     path = os.path.join(os.getcwd(), 'images')
-    sf.encode(path, 'sinica.png', 'data.txt')
+    sf.encode(path, 'sinica.png', 'data\\data_sinica.txt')
 
-
+# Фильтр для enc_sinica.png
 def test_3():
-    '''
-    Фильтр для enc_sinica.png
-    :return:
-    '''
     path = os.path.join(os.getcwd(), 'images\encoded')
     sf.pictures_lsb_colored(path, 'enc_sinica.png', 0, 0)
 
-
-def test_5():
-    '''
-    Декодирование из enc_sinica.png
-    :return:
-    '''
+# Декодирование из enc_sinica.png
+def test_4():
     path = os.path.join(os.getcwd(), 'images\\encoded')
     sf.decode(path, 'enc_sinica.png', 'result.txt')
 
+# Матожидание размера черного блока в углу 100x100 в sinica.png и enc_sinica.png
+def test_5():
+    test_black_square('sinica.png')
 
-def test_4():
-    '''
-    Матожидание размера черного блока в углу 100x100 в sinica.png и enc_sinica.png
-    :return:
-    '''
-    left = 0
-    right = 100
-    height = 100
-    width = 100
+# Шифрование в mothers_kiss.png - 5%
+def test_6():
     path = os.path.join(os.getcwd(), 'images')
-    matrix_1 = sf.picture_to_lsb_matrix_colored(path, 'sinica.png', 0, 0)
-    matrix_2 = sf.picture_to_lsb_matrix_colored(path + '\\encoded', 'enc_sinica.png', 0, 0)
-    freq_1 = sf.black_area_frequencies(matrix_1[0:width, 0:height], left, right)
-    freq_2 = sf.black_area_frequencies(matrix_2[0:width, 0:height], left, right)
-
-    try:
-        print('E_wout_st = {}'.format(sum([i*freq_1[i] for i in range(len(freq_1))]) / sum(freq_1) / width*height))
-    except ZeroDivisionError:
-        print("There aren't black blocks with size between {} and {}, so E_wout_st = 0".format(left, right))
-
-    try:
-        print('E_w_st = {}'.format(sum([i*freq_2[i] for i in range(len(freq_2))]) / sum(freq_2) / width*height))
-    except ZeroDivisionError:
-        print("There aren't black blocks with size between {} and {}, so E_w_st = 0".format(left, right))
-
-    fig = plt.figure()
-    plt.title('Рапределение черных полей')
-    plt.plot(range(left, right), freq_1, label='Without_stego')
-    plt.plot(range(left, right), freq_2, label='With_stego')
-    plt.legend()
-    plt.savefig(os.path.join(path + '\\plots', 'sinica.png'))
-    return
-
-
-def test_9():
-    '''
-    Шифрование в mothers_kiss.png - 5%
-    :return:
-    '''
-    path = os.path.join(os.getcwd(), 'images')
-    sf.encode(path, 'mothers_kiss.png', 'shir.cpp')
+    sf.encode(path, 'mothers_kiss.png', 'data\\data_mother.txt')
     return 
 
-
-def test_6():
-    '''
-    Тест фильтра
-    :return:
-    '''
+# Фильтр
+def test_7():
     path = os.path.join(os.getcwd(), 'images\\encoded')
     sf.pictures_lsb_colored(path, 'enc_mothers_kiss.png', 0, 0)
 
 
-def test_7():
-    '''
-    Хи-квадрат для стеганографии enc_mothers_kiss.png
-    :return:
-    '''
+# Хи-квадрат для стеганографии enc_mothers_kiss.png
+def test_8():
     path = os.path.join(os.getcwd(), 'images\\encoded')
     sf.chi_square_all(path, 'enc_mothers_kiss.png', 0)
     return
 
-
-def test_8():
-    '''
-    Мат. ожидание относительной площади черного блока в углу 100х100 в mothers_kiss.png и enc_mothers_kiss.png
-    :return: nothing (ahaha)
-    '''
-    left = 0
-    right = 100
-    height = 100
-    width = 100
-    path = os.path.join(os.getcwd(), 'images')
-    matrix_1 = sf.picture_to_lsb_matrix_colored(path, 'mothers_kiss.png', 0, 0)
-    matrix_2 = sf.picture_to_lsb_matrix_colored(path + '\\encoded', 'enc_mothers_kiss.png', 0, 0)
-    freq_1 = sf.black_area_frequencies(matrix_1[0:width, 0:height], left, right)
-    freq_2 = sf.black_area_frequencies(matrix_2[0:width, 0:height], left, right)
-
-    try:
-        print('E_wout_st = {}'.format((sum([i*freq_1[i] for i in range(len(freq_1))]) / sum(freq_1)) / (width*height)))
-    except ZeroDivisionError:
-        print("There aren't black blocks with size between {} and {}, so E_wout_st = 0".format(left, right))
-
-    try:
-        print('E_w_st = {}'.format((sum([i*freq_2[i] for i in range(len(freq_2))]) / sum(freq_2)) / (width*height)))
-    except ZeroDivisionError:
-        print("There aren't black blocks with size between {} and {}, so E_w_st = 0".format(left, right))
-
-    fig = plt.figure()
-    plt.title('Рапределение черных полей')
-    plt.plot(range(left, right), freq_1, label='Without_stego')
-    plt.plot(range(left, right), freq_2, label='With_stego')
-    plt.legend()
-    plt.savefig(os.path.join(path + '\\plots', 'mothers_kiss.png'))
-    return
+# Мат. ожидание относительной площади черного блока в углу 100х100 в mothers_kiss.png и enc_mothers_kiss.png
+def test_9():
+    test_black_square('mother_kiss.png')
 
 
+# Подсчет для угла 100х100 зависимости энтропии Шенона от итераций жизни
 def test_10():
-    matrix = life.read()
-    iterations = int(input())
-    frequencies = sf.count_statistics(matrix, iterations)
+    test_life_iterations('mothers_kiss.png')
+
+
+# Итерации жизни для sinica.png
+def test_11():
+    test_life_iterations('sinica.png')
+
+
+# Кодирование в betchoven.png - 95%
+def test_12():
+    path = os.path.join(os.getcwd(), 'images')
+    sf.encode(path, 'betchoven.png', 'data\\data_betchoven.txt')
+
+# Зависимость энтропии Шенона от итераций жизни
+def test_13():
+    test_life_iterations('betchoven.png')
 
 
 def main():
-    test_8()
-    return
-
+    test_13()
 
 if __name__ == '__main__':
     main()
--- sten_func.py
+++ sten_func.py
@@ -1,22 +1,15 @@
 from PIL import Image
 import numpy as np
 import os
+from collections import defaultdict
 from collections import deque
 import matplotlib.pyplot as plt
 import stego_encoded as ste
 import life
 
 
-# Создает файл - результат фильтрации последнего бита в байте
+#Создает файл - результат фильтрации последнего бита в байте 
 def pictures_lsb(path, name, ind=0):
-    '''
-    Создает файл - результат фильтрации последнего бита в байте
-    :param path:
-    :param name:
-    :param ind:
-    :return:
-    '''
-
     container = Image.open(os.path.join(path, name))
     im_size = container.size
     im_str = container.tobytes()
@@ -41,7 +34,7 @@ def lsb_to_matrix(path, name):
 
 def picture_to_lsb_matrix(path, name, ind=0):
     '''
-    Делает из картинки булевскую матрицу, вылеляя из каждого байта 
+    Делает из картинки булевскую матрицу, вылеляя из каждого байта по биту на месте ind
     '''
     container = Image.open(os.path.join(path, name))
     im_size = container.size
@@ -51,8 +44,12 @@ def picture_to_lsb_matrix(path, name, ind=0):
     result = np.resize(result, (container.size[0]*cod, container.size[1]))
     return result, cod
 
-
 def picture_to_lsb_matrix_colored(path, name, ind, color):
+    '''
+    Делает булевскую матрицу, выделяя из каждого байта,
+    отвечающего за цвет color (R - 0, G - 1, B - 2, A -3),
+    по биту на месте ind
+    '''
     container = Image.open(os.path.join(path, name))
     im_size = container.size
     im_str = container.tobytes()
@@ -61,14 +58,20 @@ def picture_to_lsb_matrix_colored(path, name, ind, color):
     result = np.resize(result, (container.size[0], container.size[1]))
     return result
 
-
 def matrix_to_picture(matrix, size, path, name):
+    '''
+    Делает из матрицы файл
+    '''
     container = Image.new('RGBA', size)
     container.frombytes(bytes(matrix), 'raw')
     container.save(os.path.join(path, name), 'png')
 
-
 def shenon_entropy(matrix):
+    '''
+    Возвращает энтропию Шенона, где случайная величина -
+    количество 0 в строке матрицы, её вероятность - частота
+    строк, содержащих x нулей, среди всех строк матрицы
+    '''
     probabilties = [0.0] * (matrix.shape[0] + 1)
     for i in matrix:
         if 0 in i:
@@ -79,8 +82,11 @@ def shenon_entropy(matrix):
     H = 0 - np.sum(np.array(probabilties) * np.log2(probabilties))
     return H
 
-
 def dfs(i, j, matrix, visited):
+    '''
+    Dfs обход матрицы, где за вершины принимаются нулевые ячейки матрицы,
+    а за концы ребер - соседние ячейки
+    '''
     stack = deque()
     stack.append((i, j))
     res = 1
@@ -101,8 +107,11 @@ def dfs(i, j, matrix, visited):
             stack.append((i, j + 1))
     return res
 
-
 def chi_square_from_frag(frag):
+    '''
+    Хи-квадрат фрагмента фотографии
+    frag - список байт рисунка
+    '''
     frequencies_1 = np.zeros(128)
     frequencies_2 = np.zeros(128)
     if len(frag) == 0:
@@ -114,7 +123,6 @@ def chi_square_from_frag(frag):
             frequencies_2[i//2] += 1
     delta = (frequencies_1 - frequencies_2)**2 / np.sum(frequencies_1 + frequencies_2)**2
     return np.sum(delta)
-
 
 def chi_square(path, name, x, y, width, height, color):
     '''
@@ -132,8 +140,13 @@ def chi_square(path, name, x, y, width, height, color):
     frag = loupe(im_str, x, y, width, height, container.size, cod)
     return chi_square_from_frag(frag)
 
-
-def chi_square_all(path, name, color):
+def chi_square_all(path, name):
+    '''
+    Разбиваем фотографию на участки 50х50, для каждого
+    считаем хи-квадрат функцию
+    path - директория, где лежит файл
+    name - имя файла
+    '''
     container = Image.open(os.path.join(path, name))
     height, width = container.size
     im_str = container.tobytes()
@@ -143,10 +156,15 @@ def chi_square_all(path, name, color):
             frag = loupe(im_str, x, y, 100, 100, (height, width), cod)
             print('chi_square for coords({0}, {1}) is {2}'.format(x, y, chi_square_from_frag(frag)))
 
+def black_area_excpection(frequencies, left, right):
+    '''
+
+    '''
+    return (sum([(i + left)*freq_1[i] for i in range(len(freq_1))]) / sum(freq_1)) / (width*height)
 
 def black_area_frequencies(matrix, left, right):
     '''
-    Функция возвращает массив frequenciesх[i from left to right] - количество встреч блоков площадью i 
+    Функция возвращает массив frequencies[i from left to right] - количество встреч блоков площадью i 
     matrix - матрица, в которой ищем площади целиком заполненые нулями
     left - левая граница индекса
     right - правая граница индекса
@@ -164,17 +182,13 @@ def black_area_frequencies(matrix, left, right):
                     frequencies[ans - left] += 1
     return frequencies
 
-
-def get_black_area_graphics(path, name_from, name_to, left, right):
-    matr = picture_to_lsb_matrix(path, name_from, 0)
-    frequencies = black_area_frequencies(matr, left, right)
-    fig = plt.figure()
-    plt.title('Распределение черных областей по lsb матрице рисунка')
-    plt.plot(range(left, right), frequencies)
-    plt.savefig(os.path.join(path, name_to))
-
-
 def encode(path, name, frm):
+    '''
+    Зашифровывает данные в картинку
+    path - директория, в которой лежит картинка
+    name - имя картинки
+    frm  - данные, которые надо передать
+    '''
     container = Image.open(os.path.join(path, name))
     im_str = container.tobytes()
     data = ste.extract_data(frm) + [0, 0]
@@ -183,8 +197,13 @@ def encode(path, name, frm):
     container.frombytes(bytes(result), 'raw')
     container.save(os.path.join(path, 'encoded\\enc_' + name), container.format)
 
-
 def decode(path, name, out):
+    '''
+    Расшифровывает данные из картинки
+    path - директоря, в которой лежит картинка
+    name - имя картинки
+    out - файл вывода спрятанной информации
+    '''
     container = Image.open(os.path.join(path, name))
     im_str = container.tobytes()
     cod = len(container.mode)
@@ -192,17 +211,15 @@ def decode(path, name, out):
     with open(out, "wb") as fout:
         fout.write(bytes(result))
 
-
 def pictures_lsb_colored(path, name, ind, color):
     '''
     Фильтрует изображение, извлекая из каждого байта,
     соответсвующего цвету color, бит с номером ind,
     и формирует новое изображение
-    :param path: путь до директории
-    :param name: имя файла
-    :param ind: номер бита
-    :param color:
-    :return:
+    path - директория, где лежит файл
+    name - имя картинки
+    ind - номер фильтруемого бита
+    color - номер байта в пикселе, из которого извлекаем бит
     '''
     container = Image.open(os.path.join(path, name))
     im_size = container.size
@@ -219,10 +236,20 @@ def pictures_lsb_colored(path, name, ind, color):
 
 
 def loupe(im_str, x, y, width, height, size,  cod=1, color=0):
+    '''
+    Возвращает фрагмент фотографии, соответсвующий участку
+    x, y - координаты левого верхнего угла фрагмента
+    width, height - ширина и высота фрагмента
+    size - размер картинки
+    cod - количество байт в пикселе
+    color - номер цвета (R - 0, G - 1, B - 2, A - 3)
+    '''
     result = list()
     p_width, p_height = size
-    if x + width > p_width or y + height > p_height:
-        return []
+    if x + width > p_width: 
+        width = p_width - x
+    if y + height > p_height:
+        height = p_height - y
     for _y in range(y, y + height):
         for _x in range(x + color, x + width*cod + color, cod):
             result.append(im_str[_y*p_width*cod + _x])
